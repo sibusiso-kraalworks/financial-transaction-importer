22ed09a70e63592ebec2687dc97924d2b1ffd248
sibusiso-kraalworks <sibusiso@kraalworks.com>
2026-03-09 13:23:01 +0200

    Restructure projects to resolve build target duplicates which was affecting tests project from running

---
+++ backend/FinancialTransactionImporter.Core/Interfaces/Authentications/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialTransactionImporter.Core.Interfaces.Authentications
+{
+    public interface IAuthService
+    {
+        Task<AuthResult> RegisterAsync(string email, string password);
+        Task<AuthResult> LoginAsync(string email, string password);
+    }
+}
